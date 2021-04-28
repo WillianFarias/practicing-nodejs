c6055e1d298ab68a113aa43b8c8cefc9f206858a
WillianFarias <williansousafarias@gmail.com>
2021-04-27 22:13:29 -0300

    finalizando a implementacao de todos os metodos http

--- src/index.js
+++ src/index.js
@@ -1,14 +1,66 @@
 const express = require('express');
+const { v4 } = require('uuid');
 
 const app = express();
 
 //use algum tipo de func que todas as rotas teram que passar
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  return response.json( {message: 'Hello World'} );
+  const { title } = request.query;
+  const result = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json( result );
 });
 
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  const project = { id: v4(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id == id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json('Project not found');
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json('Project not found');
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204);
+});
 
 
 app.listen(3333, () => {
